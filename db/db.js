d342d918e14ac57ac8170288526b921c036120ad
Tony Lancaster <lanc1968@gmail.com>
2023-04-05 20:51:41 +0100

    Swap DB in use.

--- db/db.js
+++ db/db.js
@@ -3,7 +3,8 @@ const mongoose = require("mongoose");
 const connectDB = () => {
   mongoose
     .connect(
-      `mongodb+srv://mikegreenan98:plaidPal@cluster1.qfpekly.mongodb.net/test`,
+    /*`mongodb+srv://mikegreenan98:plaidPal@cluster1.qfpekly.mongodb.net/test`,*/
+      `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@testing.4yxs9ta.mongodb.net/users`,
       { useNewUrlParser: true, useUnifiedTopology: true }
     )
     .then(() => {
